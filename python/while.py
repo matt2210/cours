2209c77103af47e05f1b39732389ba6676a00f2e
BlackWido <m2210100@outlook.fr>
2021-09-02 13:40:44 +0200

    Update While.py
    - Add Level Choise
    - Add Max Try
    - Add Max Try -> END
    - Add Restart Game
    - Add Clear Of Console when stop game ;)

--- python/while.py
+++ python/while.py
@@ -1,14 +1,46 @@
-import random
+import time
+import os
+end = 0
+while(end != 'n'):
+    tentative = 0
+    import random
+    print("Choix du niveau [1-2-3]")
+    level = int(input())
+    if(level == 1):
+        random = random.randint(1, 100)
+        print(random)
+        print("Entrer un nombre compris entre 1 et 100")
+        max_tentative = 6
+    if(level == 2):
+        random = random.randint(1, 1000)
+        print(random)
+        print("Entrer un nombre compris entre 1 et 1000")
+        max_tentative = 12
+    if(level == 3):
+        random = random.randint(1, 10000)
+        print(random)
+        print("Entrer un nombre compris entre 1 et 10000")
+        max_tentative = 18
+    number = 0
+    while (number != random and  tentative != max_tentative):
+        number = int(input())
+        tentative = tentative + 1
+        if (number < random):
+            # tentative = tentative + 1
+            restant = max_tentative - tentative
+            print("Plus Grand !")
+            print("Tentative numero", tentative, "Il vous en reste ", restant)
+        if (number > random):
+            # tentative = tentative + 1
+            restant = max_tentative - tentative
+            print("Plus Petit !")
+            print("Tentative numero", tentative, "Il vous en reste ", restant)
 
-random = random.randint(1,100)
 
-print(random)
-print("Entrer un nombre compris entre 10 et 20")
-number = int(input())
-while (number != random):
-    if(number < random):
-        print("Plus Grand !")
-    if(number > random):
-        print("Plus Petit !")
-    number = int(input())
-print("Nombre correct")
+
+    print("Partie Fini")
+    time.sleep(1)
+    print("Voulez-Vous re lancer une partie ? (Y/n)")
+    end = str(input())
+    os.system('cls')
+
