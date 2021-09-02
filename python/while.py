3d34ba448db183172ee533a2e73527812aef5564
BlackWido <m2210100@outlook.fr>
2021-09-02 11:37:05 +0200

    update While.py

--- python/while.py
+++ python/while.py
@@ -1,8 +1,14 @@
+import random
+
+random = random.randint(1,100)
+
+print(random)
 print("Entrer un nombre compris entre 10 et 20")
 number = int(input())
-while number < 10:
-    print("Plus Grand !")
+while (number != random):
+    if(number < random):
+        print("Plus Grand !")
+    if(number > random):
+        print("Plus Petit !")
     number = int(input())
-while number > 20:
-    print("Plus Petit !")
-    number = int(input())
+print("Nombre correct")
