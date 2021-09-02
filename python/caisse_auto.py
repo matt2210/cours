5ae807f157c768ad9ef6d34748b1df6e834d26d2
BlackWido <m2210100@outlook.fr>
2021-09-02 11:19:15 +0200

    Add while ex

---
+++ python/caisse_auto.py
@@ -0,0 +1,8 @@
+print("Ajouter les articles")
+somme = 0
+n = int(input())
+while (n != 0):
+    somme = somme + n
+    print(somme)
+    n = int(input())
+print("Saisie Fini.")
---
+++ python/while.py
@@ -0,0 +1,8 @@
+print("Entrer un nombre compris entre 10 et 20")
+number = int(input())
+while number < 10:
+    print("Plus Grand !")
+    number = int(input())
+while number > 20:
+    print("Plus Petit !")
+    number = int(input())
